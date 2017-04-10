6697e4b94764d9392114a50d7860c09e931645e5
Padillon <krlosx1596@gmail.com>
2017-04-09 21:25:19 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,149 @@
+/*
+	Copyright 2015 Ludwin Hernández
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    Crear cubo.
+*/
+
+#include <stdio.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+//Definimos variables
+double rotate_y=0;
+double rotate_x=0;
+double rotate_z=0;
+
+GLfloat X = 0.0f;
+GLfloat Y = 0.0f;
+GLfloat Z = 0.0f;
+GLfloat scale = 1.0f;
+
+
+void display()
+{
+    //  Borrar pantalla y Z-buffer
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+    // Resetear transformaciones
+    glLoadIdentity();
+    // Rotar en el eje X,Y y Z
+    glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+    glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+    glRotatef( rotate_z, 0.0, 0.0, 1.0 );
+    glTranslatef(X, Y, Z); 	// Transladar en los 3 ejes
+    // Otras transformaciones
+    glScalef(scale, scale, scale);
+     glColor3f(1.0, 1.0, 1.0);
+    /* Dibujamos una tetera */
+    glutWireTeapot(0.65);
+   
+
+    glFlush();
+    glutSwapBuffers();
+
+}
+
+// Función para controlar teclas especiales
+void specialKeys( int key, int x, int y )
+{
+
+    //  Flecha derecha: aumentar rotación 7 grados
+    if (key == GLUT_KEY_RIGHT)
+        rotate_y += 7;
+
+    //  Flecha izquierda: rotación en eje Y negativo 7 grados
+    else if (key == GLUT_KEY_LEFT)
+        rotate_y -= 7;
+    //  Flecha arriba: rotación en eje X positivo 7 grados
+    else if (key == GLUT_KEY_UP)
+        rotate_x += 7;
+    //  Flecha abajo: rotación en eje X negativo 7 grados
+    else if (key == GLUT_KEY_DOWN)
+        rotate_x -= 7;
+    //  Tecla especial F2 : rotación en eje Z positivo 7 grados
+    else if (key == GLUT_KEY_F2)
+        rotate_z += 7;
+    //  Tecla especial F2 : rotación en eje Z negativo 7 grados
+    else if (key == GLUT_KEY_F2)
+        rotate_z -= 7;
+
+    //  Solicitar actualización de visualización
+    glutPostRedisplay();
+
+}
+
+// Función para controlar teclas normales del teclado.
+void keyboard(unsigned char key, int x, int y)
+{
+    //control de teclas que hacen referencia a Escalar y transladar el cubo en los ejes X,Y,Z.
+    switch (key)
+    {
+    case '+':
+        scale=0.9;
+        break;
+    case '-':
+        scale=0.5;
+        break;
+    case 'x' :
+        X += 0.1f;
+        break;
+    case 'X' :
+        X -= 0.1f;
+        break;
+    case 'y' :
+        Y += 0.1f;
+        break;
+    case 'Y' :
+        Y -= 0.1f;
+        break;
+    case 'z':
+        Z -= 0.1f;
+        break;
+    case 'Z':
+        Z += 0.1f;
+        break;
+    case 'q':
+        exit(0);			// exit
+    }
+    glutPostRedisplay();
+}
+
+
+
+int main(int argc, char* argv[])
+{
+
+    //  Inicializar los parámetros GLUT y de usuario proceso
+    glutInit(&argc,argv);
+
+    // Solicitar ventana con color real y doble buffer con Z-buffer
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize (800, 600);
+    glutInitWindowPosition (0, 0);
+    // Crear ventana
+    glutCreateWindow("Cubo controlado por teclas");
+
+    // Habilitar la prueba de profundidad de Z-buffer
+    glEnable(GL_DEPTH_TEST);
+
+    // Funciones de retrollamada
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(specialKeys);
+
+    // Pasar el control de eventos a GLUT
+    glutMainLoop();
+
+    // Regresar al sistema operativo
+    return 0;
+
+}
